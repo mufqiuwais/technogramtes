2e512278e0a89e208bc8a482ab5c6817dc6f5032
mufqiuwais <mufqi.uwais.tif418@polban.ac.id>
2020-12-18 21:35:29 +0700

    idiott

--- client/technogram/src/api/const.js
+++ client/technogram/src/api/const.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://technogram-api.technogram.tech"
 // const BASE_URL = "http://localhost:3000";
-const BASE_URL = "https://polar-forest-72541.herokuapp.com/news/recent";
+const BASE_URL = "https://polar-forest-72541.herokuapp.com";
 
 const BERITA_URL = BASE_URL + "/news";
 const KATEGORI_URL = BASE_URL + "/kategori";
